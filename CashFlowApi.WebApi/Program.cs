1371b6b2fbfba1a512a15ebdaa59d05e6179faab
danilo.rovati <danilo.rovati@modalgr.com.br>
2025-05-08 22:03:48 -0300

    feat(config): Setup secrets manager (dbSecret)

--- CashFlowApi.WebApi/Program.cs
+++ CashFlowApi.WebApi/Program.cs
@@ -1,5 +1,8 @@
+using Amazon.SecretsManager.Model;
+using Amazon.SecretsManager;
 using CashFlowApi.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 var environment = builder.Environment.EnvironmentName;
@@ -8,6 +11,15 @@ if (environment.Equals("LocalDevelopment"))
 {
     builder.Services.AddDbContext<ApplicationDbContext>(options =>
         options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(new Version(8, 0, 31)))
+    );
+}
+else
+{
+    DbSecret dbSecret = await LoadDatabaseSecretAsync();
+    var connectionString = $"Server={dbSecret.Host};Port={dbSecret.Port};Database={dbSecret.Database};User Id={dbSecret.Username};Password={dbSecret.Password};";
+
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 31)))
     );
 }
 
@@ -31,3 +43,28 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+async Task<DbSecret> LoadDatabaseSecretAsync()
+{
+    using var client = new AmazonSecretsManagerClient();
+    var request = new GetSecretValueRequest
+    {
+        SecretId = "CacheFlowDbSecret" 
+    };
+
+    var response = await client.GetSecretValueAsync(request);
+    return JsonConvert.DeserializeObject<DbSecret>(response.SecretString);
+}
+
+public class DbSecret
+{
+    public string Host { get; set; }
+
+    public string Port { get; set; }
+    
+    public string Username { get; set; }
+    
+    public string Password { get; set; }
+    
+    public string Database { get; set; }
+}
