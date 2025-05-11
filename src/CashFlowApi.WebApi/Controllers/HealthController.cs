6d3be11b8d92d93e031c39b8db44d72c4ffd1b25
danilo.rovati <danilo.rovati@modalgr.com.br>
2025-05-11 00:11:07 -0300

    feat: add health endpoint

---
+++ src/CashFlowApi.WebApi/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace CashFlowApi.WebApi.Controllers;
+
+[ApiController]
+[Route("api/health")]
+[Produces("application/json")]
+public class HealthController : Controller
+{
+    [HttpGet("")]
+    [SwaggerOperation(Summary = "Check if the api is alive.")]
+    [SwaggerResponse(StatusCodes.Status200OK, "The api is health.")]
+    public IActionResult Health()
+    {
+        return Ok( new { status = "Alive" });
+    }
+}
